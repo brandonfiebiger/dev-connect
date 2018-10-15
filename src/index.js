efc4e48f900dd076f150d5484d83077dc9c1f99d
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-10-15 14:44:07 -0600

    Import router into index, wrap app in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = createStore(rootReducer, devTools);
 
 ReactDOM.render(
   <Provider store={ store }>
-   <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'));
 
