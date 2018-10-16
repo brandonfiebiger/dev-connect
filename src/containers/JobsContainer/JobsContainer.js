10f89ec62a559c3e81fdd61cde78c37f2aa0f986
Kurt Miller <kmiller9393@gmail.com>
2018-10-15 21:16:42 -0600

    Update JobsContainer so it has access to props directly from store

--- src/containers/JobsContainer/JobsContainer.js
+++ src/containers/JobsContainer/JobsContainer.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import './JobsContainer.css';
 import { JobCard } from '../../components/JobCard/JobCard';
+import './JobsContainer.css';
 
-
-export const JobsContainer = (props) => {
-  
+export const JobsContainer = props => {
   const displayJobCards = () => {
-    return props.jobs.map(job =>  <JobCard description={job.description} company={job.company} location={job.location} status={job.status} jobTitleId={job.job_title_id} jobTypes={props.jobTypes}/>)
-  }
+    return props.jobs.map(job => (
+      <JobCard
+        description={job.description}
+        company={job.company}
+        location={job.location}
+        status={job.status}
+        jobTitleId={job.job_title_id}
+        jobTypes={props.jobTypes}
+      />
+    ));
+  };
 
   return (
-    <ul>
-      {displayJobCards()}
-    </ul>
-  )
-}
+    <div>
+      <h1>Job Seekers</h1>
+      <ul>{displayJobCards()}</ul>
+    </div>
+  );
+};
 
-  export const mapStateToProps = state => ({
-    jobs: state.jobs,
-    jobTypes: state.jobTypes
-  })
+export const mapStateToProps = state => ({
+  jobs: state.jobs,
+  jobTypes: state.jobTypes
+});
 
-  export default connect(mapStateToProps, null)(JobsContainer);
+export default connect(
+  mapStateToProps,
+  null
+)(JobsContainer);
