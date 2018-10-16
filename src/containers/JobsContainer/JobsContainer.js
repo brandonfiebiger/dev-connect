9a786142eef425515727234930e63e5b3d9121f7
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-10-15 18:29:32 -0600

    Pass jobTypes down as props to JobCards to locate job_title and average_salary

--- src/containers/JobsContainer/JobsContainer.js
+++ src/containers/JobsContainer/JobsContainer.js
@@ -7,7 +7,7 @@ import { JobCard } from '../../components/JobCard/JobCard';
 export const JobsContainer = (props) => {
   
   const displayJobCards = () => {
-    return props.jobs.map(job =>  <JobCard description={job.description} company={job.company} location={job.location} status={job.status} jobTitleId={job.job_title_id}/>)
+    return props.jobs.map(job =>  <JobCard description={job.description} company={job.company} location={job.location} status={job.status} jobTitleId={job.job_title_id} jobTypes={props.jobTypes}/>)
   }
 
   return (
@@ -18,7 +18,8 @@ export const JobsContainer = (props) => {
 }
 
   export const mapStateToProps = state => ({
-    jobs: state.jobs
+    jobs: state.jobs,
+    jobTypes: state.jobTypes
   })
 
   export default connect(mapStateToProps, null)(JobsContainer);
