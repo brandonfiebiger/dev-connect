65fd641cc185330e194986100233cc83db9c87da
Kurt Miller <kmiller9393@gmail.com>
2018-10-16 13:35:38 -0600

    Update EmployerContainer container and create form for this container

--- src/containers/EmployerContainer/EmployerContainer.js
+++ src/containers/EmployerContainer/EmployerContainer.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { addNewJobType, addNewJob } from '../../actions';
 import './EmployerContainer.css';
 
-export const EmployerContainer = () => {
-  return (
-    <div>
-      <h1>Employers</h1>
-    </div>
-  );
-};
+export class EmployerContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      jobType: {}
+    };
+  }
+
+  addJobTitleOptions = () => {
+    const { jobs, jobTypes } = this.props;
+    return jobTypes.map(type => <option>{type.job_title}</option>);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Employers</h1>
+        <form>
+          <select>
+            {this.addJobTitleOptions()}
+            <option>Add New Job Title +</option>
+          </select>
+          <input name="title" />
+          <input name="salary" />
+          <input name="company" />
+          <input name="location" />
+          <input name="description" />
+          <button>Add Job</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export const mapStateToProps = state => ({
+  jobs: state.jobs,
+  jobTypes: state.jobTypes
+});
+
+export const mapDispatchToProps = dispatch => ({
+  addJobType: jobType => dispatch(addNewJobType(jobType)),
+  addJob: job => dispatch(addNewJob(job))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EmployerContainer);
