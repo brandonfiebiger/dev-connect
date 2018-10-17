0178ec856b2b2c23d5a5e2c8fd67807eff9e8e7f
Kurt Miller <kmiller9393@gmail.com>
2018-10-17 15:34:51 -0600

    Update savedJobsReducer so no linting error appears

--- src/reducers/savedJobsReducer.js
+++ src/reducers/savedJobsReducer.js
@@ -1,13 +1,14 @@
 export const savedJobsReducer = (state = [], action) => {
   switch (action.type) {
-    case 'SAVE_JOB':
-      return [...state, action.job];
-    case 'REMOVE_JOB':
-      const filteredJobs = state.filter(job => {
-        return job !== action.job;
-      });
-      return filteredJobs;
-    default:
-      return state;
+  case 'SAVE_JOB':
+    return [...state, action.job];
+  case 'REMOVE_JOB': {
+    const filteredJobs = state.filter(job => {
+      return job !== action.job;
+    });
+    return filteredJobs;
+  }
+  default:
+    return state;
   }
 };
