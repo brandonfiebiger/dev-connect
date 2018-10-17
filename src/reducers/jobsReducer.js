dca130012ade226a35559380c25e7783cf756df3
Kurt Miller <kmiller9393@gmail.com>
2018-10-17 15:34:29 -0600

    Update jobsReducer

--- src/reducers/jobsReducer.js
+++ src/reducers/jobsReducer.js
@@ -1,10 +1,10 @@
 export const jobsReducer = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_JOBS':
-      return [...action.jobs];
-    case 'ADD_NEW_JOB':
-      return [...state, action.job];
-    default:
-      return state;
+  case 'ADD_JOBS':
+    return [...action.jobs];
+  case 'ADD_NEW_JOB':
+    return [...state, action.job];
+  default:
+    return state;
   }
 };
