2e716d7179add475f8716801f69a1b7e93d1ab69
Kurt Miller <kmiller9393@gmail.com>
2018-10-16 18:02:35 -0600

    Update JobCard component so it renders company and location too

--- src/components/JobCard/JobCard.js
+++ src/components/JobCard/JobCard.js
@@ -2,5 +2,21 @@ import React from 'react';
 import './JobCard.css';
 
 export const JobCard = props => {
-  return <li>{props.description}</li>;
+  const { description, company, location, status, jobType } = props;
+  const allLocations = location.split(' ');
+  const newLocations = allLocations.map(
+    location =>
+      location.charAt(0).toUpperCase() + location.slice(1).toLowerCase()
+  );
+  const newCompany =
+    company.charAt(0).toUpperCase() + company.slice(1).toLowerCase();
+
+  console.log(newLocations);
+  return (
+    <div className="job-card">
+      <h3>{newCompany}</h3>
+      <p>{newLocations.join(' ')}</p>
+      <p>{description}</p>
+    </div>
+  );
 };
