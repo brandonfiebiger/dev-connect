8f24e326c1c900d5d4439ce22aff81a4fb481555
Kurt Miller <kmiller9393@gmail.com>
2018-10-17 16:06:37 -0600

    Add snapshot testing to JobCard.test.js

--- src/components/JobCard/JobCard.test.js
+++ src/components/JobCard/JobCard.test.js
@@ -5,9 +5,22 @@ import { JobCard, mapDispatchToProps } from './JobCard';
 describe('JobCard', () => {
   let wrapper;
   let mockCompany = 'Gusto';
-  let mockLocation = '';
+  let mockLocation = 'Denver, CO';
+  let mockType = 'UX Designer';
+  let mockDescription = 'We are looking for a talented developer';
 
   beforeEach(() => {
-    wrapper = shallow(<JobCard />);
+    wrapper = shallow(
+      <JobCard
+        description={mockDescription}
+        company={mockCompany}
+        location={mockLocation}
+        jobType={mockType}
+      />
+    );
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 });
