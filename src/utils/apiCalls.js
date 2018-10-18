0286c8018fac3af6b8253865980daf45431da872
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-10-17 19:44:31 -0600

    Add postNewJob function

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,66 +1,31 @@
-export const postNewJobAndJobType = (state) => {
-  let placholderId;
-    const {
-      jobType,
-      jobTitle,
+export const postNewJob = async (state) => {
+  const {
+    jobType,
+    jobTitle,
+    company,
+    location,
+    salary,
+    description
+  } = state;
+  const response = await fetch(process.env.REACT_APP_DATABASE_API_URL + '/api/v1/jobs', {
+    method: 'POST',
+    body: JSON.stringify({
+      description,
       company,
       location,
-      salary,
-      description
-    } = state;
-
-    fetch(process.env.REACT_APP_DATABASE_API_URL + '/api/v1/job-types', {
-      method: 'POST',
-      body: JSON.stringify({
-        job_title: jobTitle,
-        average_salary: salary
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => response.json())
-      .then(id => {
-        jobType.id = id.id;
-        this.props.addJobType({
-          job_title: jobTitle,
-          average_salary: salary,
-          id: id.id
-        });
-      })
-      .catch(error => console.log(error.error));
-
-      fetch(process.env.REACT_APP_DATABASE_API_URL + '/api/v1/jobs', {
-        method: 'POST',
-        body: JSON.stringify({
-          description,
-          company,
-          location,
-          status: 'none',
-          job_title_id: jobType.id
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(response => response.json())
-        .then(id => {
-          this.props.addJob({
-            id: id.id,
-            company,
-            description,
-            location,
-            status: 'none',
-            job_title_id: jobType.id
-          });
-        })
-        .catch(error => console.log(error));
-   
+      status: 'none',
+      job_title_id: jobType.id
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+   const jobId = await response.json();
+   return jobId.id;
 }
 
 
 export const postNewJobType = async (state) => {
-  let returnId;
   const {
     jobType,
     jobTitle,
